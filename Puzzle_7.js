953b08442b3c089c955c8bda24597b7ab0147ad1
Lbehnisch <lbehnisch@cca.edu>
2016-01-29 20:59:14 -0800

    Create Puzzle_7.js

---
+++ Puzzle_7.js
@@ -0,0 +1,59 @@
+while (true){
+   if (getColor() != "red") {
+  down();
+} else {
+  right();right();
+  while (true){
+   if (getColor() != "red") {
+  up();
+} else {
+  right();right();
+  while (true){
+   if (getColor() != "red") {
+  down();
+} else {
+  right();right();
+  while (true){
+   if (getColor() != "red") {
+  up();
+} else {
+  right();right();
+  while (true){
+   if (getColor() != "red") {
+  down();
+} else {
+  right();right();
+  while (true){
+   if (getColor() != "red") {
+  up();
+} else {
+  right();right();
+  while (true){
+   if (getColor() != "red") {
+  down();
+} else {
+  right();right();
+  while (true){
+   if (getColor() != "red") {
+  up();
+} else {
+  right();right();
+}
+}
+}
+}
+
+}
+}
+}
+}
+
+}
+}
+}
+}
+
+}
+}
+}
+}
