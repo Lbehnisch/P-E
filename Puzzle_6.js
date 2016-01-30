8b15c7e9f89738b343cc9e6151da069ce4cba44b
Lbehnisch <lbehnisch@cca.edu>
2016-01-29 20:59:59 -0800

    Create Puzzle_6.js

---
+++ Puzzle_6.js
@@ -0,0 +1,21 @@
+right();
+
+var blue=getColor();
+
+
+if (getColor() == "blue") {
+  down(); down();down();
+  left();
+  down();
+} else {
+	up();up();up();
+
+}
+
+var blue=getColor();
+
+
+if (getColor() == "blue") {
+ right(); up();
+} else {
+	left(); up();
